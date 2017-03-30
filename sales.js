6fab2288bad369c64ceefa983826b22ed15de98a
Cborrero1 <Cborrero1@mail.csuchico.edu>
2017-03-30 11:46:44 -0700

    Add files via upload

--- sales.js
+++ sales.js
@@ -2,21 +2,20 @@ var runningTotal = 0.0;
 
 function addItem()
 {
-  var newItem;
-  newItem = document.getElementById("price").value;
-  //IF newItem is not a number
-  // THEN show an alert: "Enter price as a number"
-  //OTHERWISE,
-  // update newItem to its value cast as a number
-  // update runningTotal to be its value plus newItem
-  // create a variable called dollars
-  // call asCurrency() by with the value of runningTotal and assign the return value to dollars
-  // update the innerHTML of the span with the id "subtotal" to be dollars
-  // update the value of the input with the id "price" to be an empty string
+  var newItem = document.getElementById("price").value;
+  if(isNaN(newItem) === true) {
+    alert("Enter price as a number");
+  }
+  else {
+  runningTotal = runningTotal + Number(newItem);
+  var dollars = asCurrency(runningTotal);
+  document.getElementById("subtotal").innerHTML = dollars;
+  document.getElementById("price").value = "";
+  setCookie("preTax",runningTotal,30);
+  }
   // update a cookie called "preTax" with the value of runningTotal
 }
 
-//takes a number and gives a string with the number displayed as USD currency
 function asCurrency(val)
 {
   return "$" + val.toFixed(2);
@@ -38,9 +37,18 @@ function getCookie(cname) {
         while (c.charAt(0) == ' ') {
             c = c.substring(1);
         }
-        if (c.indexOf(name) == 0) {
+        if (c.indexOf(name) === 0) {
             return c.substring(name.length, c.length);
         }
     }
     return "";
+}
+
+function calculateReceipt() {
+  var receiptSubtotal = Number(getCookie("preTax"));
+  var receiptTax = receiptSubtotal * 0.075;
+  var receiptTotal = receiptSubtotal + receiptTax;
+  document.GetElementById("sub").innerHTML = asCurrency(receiptSubtotal);
+  document.GetElementById("tax").innerHTML = asCurrency(receiptTax);
+  document.GetElementById("tot").innerHTML = asCurrency(receiptTotal);
 }
